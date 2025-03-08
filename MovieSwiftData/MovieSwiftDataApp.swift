15a3f5d2f50b50f752870351ec3acb6d91b4621c
superkookai <weerawut@live.com>
2025-03-08 13:19:44 +0700

    Initial Commit

---
+++ MovieSwiftData/MovieSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftDataApp.swift
+//  MovieSwiftData
+//
+//  Created by Weerawut Chaiyasomboon on 08/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
